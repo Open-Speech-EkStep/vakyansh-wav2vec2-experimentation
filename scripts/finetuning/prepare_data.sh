9c9325c866107f99b2a05ca21cdd2fcc510d3d70
PS <ps@PSs-MacBook-Pro.local>
2021-01-21 16:30:35 +0530

    Priyanshi|Neeraj: Added Start Finetuning script and config

---
+++ scripts/finetuning/prepare_data.sh
@@ -0,0 +1,27 @@
+
+### Values to change -start ###
+
+prep_scripts="/home/harveen.chadha/common_scripts/prep_scripts"
+data_path="/home/jupyter/"
+valid_percentage=0.04
+
+### Values to change end ###
+
+
+dir=$PWD
+parentdir="$(dirname "$dir")"
+parentdir="$(dirname "$parentdir")"
+
+destination_path=$parentdir'/data/finetuning'
+
+
+python ${prep_scripts}/manifest.py ${data_path} --dest ${destination_path} --ext wav --train-name train --valid-percent ${valid_percentage} --jobs -1
+echo "Manifest Creation Done"
+
+python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/train.tsv --output-dir ${destination_path} --output-name train --txt-dir ${data_path}
+python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/valid.tsv --output-dir ${destination_path} --output-name valid --txt-dir ${data_path}
+
+echo "Word file generated"
+
+python ${prep_scripts}/dict_and_lexicon_maker.py --wrd ${destination_path}/train.wrd --lexicon ${destination_path}/lexicon.lst --dict ${destination_path}/dict.ltr.txt
+echo "Dict file generated"
---
+++ scripts/finetuning/start_finetuning.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+
+### Values to change - start ###
+
+config_name='finetuning.yaml'
+gpus=4
+run_in_nohup=0  #0 for no, 1 for yes
+### Values to change - end ###
+
+dir=$PWD/
+parentdir="$(dirname "$dir")"
+parentdir="$(dirname "$parentdir")"
+printf "** Directory to code is: $parentdir"
+
+config_path=${parentdir}'/config/'${config_name}
+data_path=${parentdir}'/data/finetuning'
+checkpoints_path=${parentdir}'/checkpoints/finetuning'
+log_path=${parentdir}'/logs/finetuning'
+tensorboard_path=${log_path}'/tensorboard'
+petrained_model_path=${parentdir}'/checkpoints/pretraining/checkpoint_best.pt'
+update_freq=$((24/${gpus}))
+
+printf "\n** Config path is: $config_path/$config_name.yaml"
+printf "\n** Data path is: $data_path"
+printf "\n** Checkpoint will be saved at: $checkpoints_path"
+printf "\n** Logs will be saved at: ${log_path}"
+printf "\n** Update frequency is: ${update_freq}"
+
+timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S" # current time
+}
+
+# importing parse_yaml.sh file
+. ${parentdir}'/scripts/'parse_yaml.sh
+eval $(parse_yaml $config_path)
+
+if [ "${run_in_nohup}" = 1 ]; then
+
+    local_timestamp=$(timestamp)
+    tensorboard_path=${tensorboard_path}_${local_timestamp}
+    mkdir -p ${tensorboard_path}
+
+    printf "\n** Tensorboard is running **"
+    printf "\n** Tensorboard logs path: ${tensorboard_path}"
+    printf "\n"
+
+    nohup python train.py --distributed-world-size ${gpus} --distributed-port ${distributed_port} $data_path \
+    --save-dir ${checkpoints_path} --fp16  --post-process ${post_process}\
+    --valid-subset ${valid_subset} --no-epoch-checkpoints --best-checkpoint-metric ${best_checkpoint_metric} --num-workers ${num_workers} \
+    --max-update ${max_update} --sentence-avg --task ${task} --arch ${arch} \ 
+    --w2v-path ${petrained_model_path}   --labels ${labels} \
+    --apply-mask --mask-selection ${mask_selection} --mask-other ${mask_other} --mask-length ${mask_length} --mask-prob ${mask_prob} \
+    --layerdrop ${layerdrop}  --mask-channel-selection ${mask_channel_selection} --mask-channel-other ${mask_channel_other} --mask-channel-length ${mask_channel_length} \
+    --mask-channel-prob ${mask_channel_prob} --zero-infinity  --feature-grad-mult ${feature_grad_mult} --freeze-finetune-updates ${freeze_finetune_updates} \
+    --validate-after-updates ${validate_after_updates} --optimizer ${optimizer} --adam-betas ${adam_betas} --adam-eps ${adam_eps} \
+    --lr ${lr} --lr-scheduler ${lr_scheduler} --warmup-steps ${warmup_steps} --hold-steps ${hold_steps} --decay-steps ${decay_steps} \
+    --final-lr-scale ${final_lr_scale} --final-dropout ${final_dropout} --dropout ${dropout} --activation-dropout ${activation_dropout} --criterion ${criterion} \
+    --attention-dropout ${attention_dropout} --max-tokens ${max_tokens} --seed ${seed}  --log-format ${log_format} --log-interval ${log_interval} \
+    --ddp-backend ${ddp_backend} --update-freq ${update_freq} \
+    --tensorboard-logdir ${tensorboard_path}  &> ${log_path}/${local_timestamp}.out & 
+
+     
+else
+    python train.py --distributed-world-size ${gpus} --distributed-port ${distributed_port} $data_path \
+    --save-dir ${checkpoints_path} --fp16  --post-process ${post_process}\
+    --valid-subset ${valid_subset} --no-epoch-checkpoints --best-checkpoint-metric ${best_checkpoint_metric} --num-workers ${num_workers} \
+    --max-update ${max_update} --sentence-avg --task ${task} --arch ${arch} \ 
+    --w2v-path ${petrained_model_path}   --labels ${labels} \
+    --apply-mask --mask-selection ${mask_selection} --mask-other ${mask_other} --mask-length ${mask_length} --mask-prob ${mask_prob} \
+    --layerdrop ${layerdrop}  --mask-channel-selection ${mask_channel_selection} --mask-channel-other ${mask_channel_other} --mask-channel-length ${mask_channel_length} \
+    --mask-channel-prob ${mask_channel_prob} --zero-infinity  --feature-grad-mult ${feature_grad_mult} --freeze-finetune-updates ${freeze_finetune_updates} \
+    --validate-after-updates ${validate_after_updates} --optimizer ${optimizer} --adam-betas ${adam_betas} --adam-eps ${adam_eps} \
+    --lr ${lr} --lr-scheduler ${lr_scheduler} --warmup-steps ${warmup_steps} --hold-steps ${hold_steps} --decay-steps ${decay_steps} \
+    --final-lr-scale ${final_lr_scale} --final-dropout ${final_dropout} --dropout ${dropout} --activation-dropout ${activation_dropout} --criterion ${criterion} \
+    --attention-dropout ${attention_dropout} --max-tokens ${max_tokens} --seed ${seed}  --log-format ${log_format} --log-interval ${log_interval} \
+    --ddp-backend ${ddp_backend} --update-freq ${update_freq} \
+    --tensorboard-logdir ${tensorboard_path}  
+
+
+fi
