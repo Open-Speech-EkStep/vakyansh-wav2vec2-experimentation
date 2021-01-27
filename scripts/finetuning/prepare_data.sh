d34f4a491d73358fe04b9eb8284e1231e46156f7
PS <ps@PSs-MacBook-Pro.local>
2021-01-27 11:01:27 +0530

    Neeraj|Priyanshi: Refactored Prepare data script

--- scripts/finetuning/prepare_data.sh
+++ scripts/finetuning/prepare_data.sh
@@ -1,25 +1,22 @@
-
-### Values to change -start ###
-
-prep_scripts="/home/harveen.chadha/common_scripts/prep_scripts"
-data_path="/home/jupyter/IITM_NPTEL_clean_english_data/test"
-valid_percentage=0.04
-
-### Values to change end ###
-
-
 dir=$PWD
 parentdir="$(dirname "$dir")"
 parentdir="$(dirname "$parentdir")"
 
+### Values to change -start ###
+
+valid_percentage=0.04
+prep_scripts="/home/harveen.chadha/common_scripts/prep_scripts"
 destination_path=$parentdir'/data/finetuning'
+wav_path="/home/jupyter/IITM_NPTEL_clean_english_data/test"
+txt_path=${wav_path}
 
+### Values to change end ###
 
-python ${prep_scripts}/manifest.py ${data_path} --dest ${destination_path} --ext wav --train-name train --valid-percent ${valid_percentage} --jobs -1
+python ${prep_scripts}/manifest.py ${wav_path} --dest ${destination_path} --ext wav --train-name train --valid-percent ${valid_percentage} --jobs -1
 echo "Manifest Creation Done"
 
-python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/train.tsv --output-dir ${destination_path} --output-name train --txt-dir ${data_path}
-python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/valid.tsv --output-dir ${destination_path} --output-name valid --txt-dir ${data_path}
+python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/train.tsv --output-dir ${destination_path} --output-name train --txt-dir ${txt_path}
+python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/valid.tsv --output-dir ${destination_path} --output-name valid --txt-dir ${txt_path}
 
 echo "Word file generated"
 
