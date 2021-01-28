1a980a179b828b84bdbcddf2c78c93983b1b55c6
neerajchhimwal <priyanshi.shah@packer-5f02b313-af67-a5bc-f344-7601fd21dc4b.c.deeplearning-platform.internal>
2021-01-28 12:13:25 +0000

    Priyanshi|Neeraj:Refactored Scripts for custom model and single inference

--- scripts/inference/generate_model.sh
+++ scripts/inference/generate_model.sh
@@ -6,8 +6,9 @@ parentdir="$(dirname "$parentdir")"
 
 finetuned_model_path=$parentdir'/checkpoints/finetuning/checkpoint_best.pt'
 custom_model_path=$parentdir'/checkpoints/custom_model/final_model.pt'
-dictionary=$parentdir'data/finetuning/dict.ltr.txt'
+dictionary=$parentdir'/data/finetuning/dict.ltr.txt'
 
 ### Values to change end ###
-
+printf "Generating Custom Model"
 python generate_model.py -f ${finetuned_model_path} -d ${dictionary} -o $custom_model_path
+printf "\nModel saved at :"${custom_model_path}"\n"
--- scripts/inference/single_file_inference.sh
+++ scripts/inference/single_file_inference.sh
@@ -4,7 +4,7 @@ parentdir="$(dirname "$parentdir")"
 
 ### Values to change, if any ###
 custom_model_path=$parentdir'/checkpoints/custom_model/final_model.pt'
-dictionary=$parentdir'data/finetuning/dict.ltr.txt'
+dictionary=$parentdir'/data/finetuning/dict.ltr.txt'
 wav_file_path="/home/jupyter/dev_english/dev_IITM/kol_134_long_219_eng-010000-013808-2-1.wav"
 decoder="viterbi"
 cuda=True
