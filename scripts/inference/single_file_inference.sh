f97b993267f9036b61164d42e919460303e8de85
PS <ps@PSs-MacBook-Pro.local>
2021-01-28 16:51:47 +0530

    Priyanshi|Neeraj: Added bash script for custom model and Refactored code

---
+++ scripts/inference/single_file_inference.sh
@@ -0,0 +1,15 @@
+dir=$PWD
+parentdir="$(dirname "$dir")"
+parentdir="$(dirname "$parentdir")"
+
+### Values to change, if any ###
+custom_model_path=$parentdir'/checkpoints/custom_model/final_model.pt'
+dictionary=$parentdir'data/finetuning/dict.ltr.txt'
+wav_file_path="/home/jupyter/dev_english/dev_IITM/kol_134_long_219_eng-010000-013808-2-1.wav"
+decoder="viterbi"
+cuda=True
+
+### Values to change end ###
+
+
+python single_file_inference.py -m ${custom_model_path} -d ${dictionary} -w ${wav_file_path} -c ${cuda} -D ${decoder}
