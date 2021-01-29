5fe9ff61a32967c4eea7fa0b9bae5915b4e4414e
PS <ps@PSs-MacBook-Pro.local>
2021-01-29 11:10:20 +0530

    Priyanshi|Neeraj: Adding lm scripts

---
+++ scripts/lm/concatenate_text.py
@@ -0,0 +1,25 @@
+from glob import glob
+from tqdm import tqdm
+import argparse
+
+def concatenate_text(txt_files_dir,combined_txt_file_save_path):
+	txt_list = []
+	all_txt = glob.glob(txt_files_dir+'**/*.txt', recursive=True)
+	for file in tqdm(all_txt):
+		with open(file) as f:
+			txt_list.append(f.read())
+
+	txt_str = '\n'.join(txt_list)
+	with open(combined_txt_file_save_path, 'w+') as f:
+		f.write(txt_str)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Run')
+    parser.add_argument('--txt-files-dir', type=str)
+    parser.add_argument('--final-txt-file-save-path', type=str)
+	args_local = parser.parse_args()
+	
+	txt_files_dir = args_local.txt_files_dir
+	combined_txt_file_save_path=args_local.combined_txt_file_save_path
+
+	concatenate_text(txt_files_dir,combined_txt_file_save_path)
---
+++ scripts/lm/make_lexicon_lst.py
@@ -0,0 +1,24 @@
+from glob import glob
+from tqdm import tqdm
+
+
+def make_lexicon_lst_from_txt_file(txt_file, path_to_save_lexicon):
+	
+	with open(txt_file) as f:
+		content_list = f.readlines()
+
+	content_list=[i.replace("\n","") for i in content_list]
+
+	with open(path_to_save_lexicon,"w+") as f:
+		for line in tqdm(content_list):
+			print(str(line+"\t"+" ".join(list(line.replace("/n","").replace(" ","|").strip()))+" |"),file=f)
+	
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Run')
+    parser.add_argument('--txt-file', type=str)
+    parser.add_argument('--path-to-save-lexicon', type=str)
+	args_local = parser.parse_args()
+	txt_file = args_local.txt_file
+	path_to_save_lexicon = args_local.path_to_save_lexicon
+	make_lexicon_lst_from_txt_file(txt_file, path_to_save_lexicon)
