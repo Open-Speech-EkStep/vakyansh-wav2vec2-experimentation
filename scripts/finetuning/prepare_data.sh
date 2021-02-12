ce5ccac8c74948db6c5e2f189a9c03b0b6da4000
neerajchhimwal <priyanshi.shah@packer-5f02b313-af67-a5bc-f344-7601fd21dc4b.c.deeplearning-platform.internal>
2021-02-12 10:28:22 +0000

    Added analysis after manifest tsv creation

--- scripts/finetuning/prepare_data.sh
+++ scripts/finetuning/prepare_data.sh
@@ -4,21 +4,30 @@ parentdir="$(dirname "$parentdir")"
 
 ### Values to change -start ###
 
-valid_percentage=0.04
-prep_scripts="../../utils/prep_scripts"
+utils="../../utils"
+prep_scripts=$utils"/prep_scripts/"
+analysis_scripts=$utils"/analysis/"
 destination_path=$parentdir'/data/finetuning'
-wav_path="/wav_dir_path"
+wav_path="/home/jupyter/dev_english/dev_IITM/"
 txt_path=${wav_path}
-
+train_name="train"
+valid_name="valid"
+valid_percentage=0.04
 ### Values to change end ###
 
-python ${prep_scripts}/manifest.py ${wav_path} --dest ${destination_path} --ext wav --train-name train --valid-percent ${valid_percentage} --jobs -1
+python ${prep_scripts}/manifest.py ${wav_path} --dest ${destination_path} --ext wav --train-name ${train_name} --valid-percent ${valid_percentage} --jobs -1
 echo "Manifest Creation Done"
 
-python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/train.tsv --output-dir ${destination_path} --output-name train --txt-dir ${txt_path}
-python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/valid.tsv --output-dir ${destination_path} --output-name valid --txt-dir ${txt_path}
+python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/${train_name}.tsv --output-dir ${destination_path} --output-name ${train_name} --txt-dir ${txt_path}
+python ${prep_scripts}/labels.py --jobs 64 --tsv ${destination_path}/${valid_name}.tsv --output-dir ${destination_path} --output-name ${valid_name} --txt-dir ${txt_path}
 
 echo "Word file generated"
 
-python ${prep_scripts}/dict_and_lexicon_maker.py --wrd ${destination_path}/train.wrd --lexicon ${destination_path}/lexicon.lst --dict ${destination_path}/dict.ltr.txt
-echo "Dict file generated"
+python ${prep_scripts}/dict_and_lexicon_maker.py --wrd ${destination_path}/${train_name}.wrd --lexicon ${destination_path}/lexicon.lst --dict ${destination_path}/dict.ltr.txt
+echo "Dict file generated from train data"
+
+echo "Starting analysis of train tsv..."
+python ${analysis_scripts}/generate_wav_report_from_tsv.py --tsv ${destination_path}/${train_name}.tsv
+echo "Starting analysis of valid tsv..."
+python ${analysis_scripts}/generate_wav_report_from_tsv.py --tsv ${destination_path}/${valid_name}.tsv
+echo "Analysis done."
--- scripts/pretraining/prepare_data.sh
+++ scripts/pretraining/prepare_data.sh
@@ -5,11 +5,16 @@ parentdir="$(dirname "$parentdir")"
 ### Values to change -start ###
 
 valid_percentage=0.04
-prep_scripts="../../utils/prep_scripts"
-destination_path=$parentdir'/data/pretraining'
-wav_path="wav_dir_path"
-
+utils="../../utils"
+prep_scripts=$utils"/prep_scripts/"
+analysis_scripts=$utils"/analysis/"
+destination_path=$parentdir"/data/pretraining"
+wav_path="/home/jupyter/dev_english/dev_IITM/"
+train_name="train"
 ### Values to change end ###
 
-python ${prep_scripts}/manifest.py ${wav_path} --dest ${destination_path} --ext wav --train-name train --valid-percent ${valid_percentage} --jobs -1
+python ${prep_scripts}/manifest.py ${wav_path} --dest ${destination_path} --ext wav --train-name ${train_name} --valid-percent ${valid_percentage} --jobs -1
 echo "Manifest Creation Done"
+echo "Starting analysis..."
+python ${analysis_scripts}/generate_wav_report_from_tsv.py --tsv ${destination_path}/${train_name}.tsv
+echo "Analysis done."
