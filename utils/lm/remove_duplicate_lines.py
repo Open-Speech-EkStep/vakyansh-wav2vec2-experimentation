eaff3212a9ce0b15a603936f7746143cc37c3588
neerajchhimwal <30681809+neerajchhimwal@users.noreply.github.com>
2021-02-25 14:54:17 +0530

    Fixed remove_duplicate_lines.py. Now calling correct method and displaying progress.

--- utils/lm/remove_duplicate_lines.py
+++ utils/lm/remove_duplicate_lines.py
@@ -1,6 +1,7 @@
-## Usage: python remove_duplicate_lines.py --inp_text_file /path/to/duplicate/lines/file --out_text_file /path/to/unique/lines/file
+## Usage: python remove_duplicate_lines.py --inp-text-file /path/to/duplicate/lines/file --out-text-file /path/to/unique/lines/file
 
 import argparse
+from tqdm import tqdm
 
 def remove_duplicate(args):
     '''
@@ -9,7 +10,7 @@ def remove_duplicate(args):
     with open(args.inp_text_file, mode='r', encoding='UTF-8') as inp_file, open(args.out_text_file, mode='w+', encoding='UTF-8') as out_file:
         lines = [line.strip() for line in inp_file.readlines()]
         lines_seen = set()
-        for line in lines:
+        for line in tqdm(lines):
             if line not in lines_seen:
                 print(line, file=out_file)
                 lines_seen.add(line)
@@ -19,4 +20,4 @@ if __name__ == '__main__':
     parser.add_argument('--inp-text-file', type=str)
     parser.add_argument('--out-text-file', type=str)
     args = parser.parse_args()
-    clean_text(args)
+    remove_duplicate(args)
