66ef4354315e17d77b11885e0b97a696db557d0f
PS <ps@PSs-MacBook-Pro.local>
2021-03-02 15:12:36 +0530

    Neeraj,Priyanshi| Added folder name functionality for data prep and infer

--- scripts/inference/infer.sh
+++ scripts/inference/infer.sh
@@ -4,31 +4,34 @@ parentdir="$(dirname "$dir")"
 parentdir="$(dirname "$parentdir")"
 
 ### Values to change -start ###
+
 w2l_decoder_viterbi=1 # 1 for viterbi, 0 for kenlm
-data_path=${parentdir}'/data/inference/'
-validation_dataset=''
-subset='test'
+inference_data_name=''
 beam=128 # 128 or 1024
+subset='test'
 
 # FOR LM MODEL
-lm_model_path=${parentdir}'/lm/lm.binary'
-lexicon_lst_path=${parentdir}'/lm/lexicon.lst'
+lm_name=''
+lm_model_path=${parentdir}'/lm/'${lm_name}'/lm.binary'
+lexicon_lst_path=${parentdir}'/lm/'${lm_name}'/lexicon.lst'
+
 ### Values to change end ###
 
 wav2vec_repo_path=${parentdir}'/../fairseq/'
 checkpoint_path=${parentdir}'/checkpoints/finetuning/checkpoint_best.pt'
 result_path=${parentdir}'/results'
+data_path=${parentdir}'/data/inference/'
 
 if [ "${w2l_decoder_viterbi}" = 1 ]; then
   python ${wav2vec_repo_path}examples/speech_recognition/infer.py ${data_path} --task audio_pretraining \
-  --nbest 1 --path ${checkpoint_path} --gen-subset ${subset} --results-path ${result_path}/${validation_dataset} --w2l-decoder viterbi \
+  --nbest 1 --path ${checkpoint_path} --gen-subset ${subset} --results-path ${result_path}/${inference_data_name} --w2l-decoder viterbi \
   --lm-weight 2 --word-score -1 --sil-weight 0 --criterion ctc --labels ltr --max-tokens 4000000 \
   --post-process letter
 
 
 else
   python ${wav2vec_repo_path}examples/speech_recognition/infer.py ${data_path} --task audio_pretraining \
-  --nbest 1 --path ${checkpoint_path} --gen-subset ${subset} --results-path ${result_path}/${validation_dataset}_kenlm_${beam} --w2l-decoder kenlm --lm-model ${lm_model_path}\
+  --nbest 1 --path ${checkpoint_path} --gen-subset ${subset} --results-path ${result_path}/${inference_data_name}_kenlm_${beam} --w2l-decoder kenlm --lm-model ${lm_model_path}\
   --lm-weight 2 --word-score -1 --sil-weight 0 --criterion ctc --labels ltr --max-tokens 4000000 --lexicon ${lexicon_lst_path} \
   --post-process letter --beam ${beam}
 
--- scripts/inference/prepare_data.sh
+++ scripts/inference/prepare_data.sh
@@ -1,16 +1,16 @@
-
 dir=$PWD
 parentdir="$(dirname "$dir")"
 parentdir="$(dirname "$parentdir")"
-### Values to change -start ###
 
-prep_scripts="../../utils/prep_scripts"
-destination_path=$parentdir'/data/inference'
+### Values to change -start ###
+inference_data_name=""
 wav_path="path_to_data/"
-txt_path=${wav_path}
+prep_scripts="../../utils/prep_scripts"
+destination_path=$parentdir'/data/inference/'${inference_data_name}
 ### Values to change end ###
 
 finetuning_dict=$parentdir'/data/finetuning/dict.ltr.txt'
+txt_path=${wav_path}
 
 python ${prep_scripts}/manifest.py ${wav_path} --dest ${destination_path} --ext wav --train-name test --valid-percent 0 --jobs -1
 echo "Manifest Creation Done"
@@ -21,10 +21,10 @@ echo "Word file generated"
 python ${prep_scripts}/dict_and_lexicon_maker.py --wrd ${destination_path}/test.wrd --lexicon ${destination_path}/lexicon.lst --dict ${destination_path}/dict.ltr.txt
 echo "Dict file generated"
 
-echo "Renaming dict.ltr.txt made from ", ${txt_path}, "to: test_dict.ltr.txt"
-mv dict.ltr.txt test_dict.ltr.txt
+# echo "Renaming dict.ltr.txt made from ", ${txt_path}, "to: test_dict.ltr.txt"
+# mv dict.ltr.txt test_dict.ltr.txt
 
-echo "Copying dict.ltr.txt used for training to", ${destination_path}
+echo "Copying ",${finetuning_dict}," used for training to ", ${destination_path}
 cp ${finetuning_dict} ${destination_path}
 
 
