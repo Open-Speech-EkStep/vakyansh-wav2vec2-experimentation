6d8c5ddd89b8fdf5a46382924951acf032def761
PS <ps@PSs-MacBook-Pro.local>
2021-03-15 10:56:32 +0530

    Priyanshi, Neeraj | Adding dictionary to custom model path

--- scripts/inference/generate_custom_model.sh
+++ scripts/inference/generate_custom_model.sh
@@ -3,12 +3,17 @@ parentdir="$(dirname "$dir")"
 parentdir="$(dirname "$parentdir")"
 
 ### Values to change, if any ###
-
+custom_model_name='final_model.pt'
 finetuned_model_path=$parentdir'/checkpoints/finetuning/checkpoint_best.pt'
-custom_model_path=$parentdir'/checkpoints/custom_model/final_model.pt'
+custom_model_path=$parentdir'/checkpoints/custom_model/'
 dictionary=$parentdir'/data/finetuning/dict.ltr.txt'
 
 ### Values to change end ###
-printf "Generating Custom Model"
-python ../../utils/inference/generate_custom_model.py -f ${finetuned_model_path} -d ${dictionary} -o $custom_model_path
-printf "\nModel saved at :"${custom_model_path}"\n"
+echo "Generating Custom Model"
+python ../../utils/inference/generate_custom_model.py -f ${finetuned_model_path} -d ${dictionary} -o ${custom_model_path}${custom_model_name}
+echo "\nModel saved at :"${custom_model_path}"\n"
+
+echo "Copying finetuning dictionary from: "${dictionary} " to: "${custom_model_path}
+cp ${dictionary} ${custom_model_path}
+echo "Completed"
+
