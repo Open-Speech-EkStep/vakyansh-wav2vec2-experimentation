ea475ace163f4cc82e00481c41a6c9bfca552f0c
Harveen <Harveen>
2021-04-28 10:50:23 +0000

    Harveen | Hydra fix inferencing scripts

--- scripts/inference/generate_custom_model.sh
+++ scripts/inference/generate_custom_model.sh
@@ -5,12 +5,13 @@ parentdir="$(dirname "$parentdir")"
 ### Values to change, if any ###
 custom_model_name='final_model.pt'
 finetuned_model_path=$parentdir'/checkpoints/finetuning/checkpoint_best.pt'
+pretrained_model_path=$parentdir'/checkpoints/pretraining/checkpoint_best.pt'
 custom_model_path=$parentdir'/checkpoints/custom_model/'
 dictionary=$parentdir'/data/finetuning/dict.ltr.txt'
 
 ### Values to change end ###
 echo "Generating Custom Model"
-python ../../utils/inference/generate_custom_model.py -f ${finetuned_model_path} -d ${dictionary} -o ${custom_model_path}${custom_model_name}
+python ../../utils/inference/generate_custom_model.py -f ${finetuned_model_path} -p ${pretrained_model_path} -d ${dictionary} -o ${custom_model_path}${custom_model_name}
 echo "\nModel saved at :"${custom_model_path}"\n"
 
 echo "Copying finetuning dictionary from: "${dictionary} " to: "${custom_model_path}
--- scripts/inference/single_file_inference.sh
+++ scripts/inference/single_file_inference.sh
@@ -5,12 +5,13 @@ parentdir="$(dirname "$parentdir")"
 ### Values to change, if any ###
 custom_model_path=$parentdir'/checkpoints/custom_model/final_model.pt'
 dictionary=$parentdir'/data/finetuning/dict.ltr.txt'
-wav_file_path="" # path to single wav file
-decoder="viterbi" # viterbi or kenlm
+wav_file_path="/home/harveen.chadha/files/code/004-M-23_001.wav" # path to single wav file
+decoder="kenlm" # viterbi or kenlm
 cuda=True
+half=True
 
 #If kenlm
-lm_name=''
+lm_name='hindi'
 lm_model_path=${parentdir}'/lm/'${lm_name}'/lm.binary'
 lexicon_lst_path=${parentdir}'/lm/'${lm_name}'/lexicon.lst'
 
@@ -18,7 +19,7 @@ lexicon_lst_path=${parentdir}'/lm/'${lm_name}'/lexicon.lst'
 
 if [ "$decoder" = "viterbi" ]
 then
-	python ../../utils/inference/single_file_inference.py -m ${custom_model_path} -d ${dictionary} -w ${wav_file_path} -c ${cuda} -D ${decoder}
+	python ../../utils/inference/single_file_inference.py -m ${custom_model_path} -d ${dictionary} -w ${wav_file_path} -c ${cuda} -D ${decoder} -H ${half}
 else
-	python ../../utils/inference/single_file_inference.py -m ${custom_model_path} -d ${dictionary} -w ${wav_file_path} -c ${cuda} -D ${decoder} -l ${lexicon_lst_path} -L ${lm_model_path}
+	python ../../utils/inference/single_file_inference.py -m ${custom_model_path} -d ${dictionary} -w ${wav_file_path} -c ${cuda} -D ${decoder} -H ${half} -l ${lexicon_lst_path} -L ${lm_model_path}
 fi
