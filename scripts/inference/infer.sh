c95e869ff1867a2c87c61abd5bac0c6d43e446e1
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2021-06-25 11:46:11 +0530

    Harveen | Update Max tokens

--- scripts/inference/infer.sh
+++ scripts/inference/infer.sh
@@ -17,8 +17,8 @@ lexicon_lst_path=${parentdir}'/lm/'${lm_name}'/lexicon.lst'
 
 # WARNING ONLY FOR OLD MODELS
 
-#pretrained_model_path=''
-pretrained_model_path=${parentdir}'/checkpoints/pretraining/checkpoint_best.pt'
+pretrained_model_path=''
+#pretrained_model_path=${parentdir}'/checkpoints/pretraining/checkpoint_best.pt'
 
 # WARNING END
 
@@ -38,7 +38,7 @@ if [ "${w2l_decoder_viterbi}" = 1 ]; then
   mkdir -p ${result_path}
   python ../../utils/inference/infer.py ${data_path} --task audio_pretraining \
   --nbest 1 --path ${checkpoint_path} --gen-subset ${subset} --results-path ${result_path} --w2l-decoder viterbi \
-  --lm-weight 2 --word-score -1 --sil-weight 0 --criterion ctc --labels ltr --max-tokens 4000000 \
+  --lm-weight 2 --word-score -1 --sil-weight 0 --criterion ctc --labels ltr --max-tokens 6000000 \
   --post-process letter
 
   python ../../utils/wer/wer_wav2vec.py -o ${result_path}/ref.word-checkpoint_best.pt-test.txt -p ${result_path}/hypo.word-checkpoint_best.pt-test.txt \
@@ -48,7 +48,7 @@ else
 
   python ../../utils/inference/infer.py ${data_path} --task audio_pretraining \
   --nbest 1 --path ${checkpoint_path} --gen-subset ${subset} --results-path ${result_path}_kenlm_${beam} --w2l-decoder kenlm --lm-model ${lm_model_path}\
-  --lm-weight 2 --word-score -1 --sil-weight 0 --criterion ctc --labels ltr --max-tokens 4000000 --lexicon ${lexicon_lst_path} \
+  --lm-weight 2 --word-score -1 --sil-weight 0 --criterion ctc --labels ltr --max-tokens 6000000 --lexicon ${lexicon_lst_path} \
   --post-process letter --beam ${beam}
 
   python ../../utils/wer/wer_wav2vec.py -o ${result_path}_kenlm_${beam}/ref.word-checkpoint_best.pt-test.txt -p ${result_path}_kenlm_${beam}/hypo.word-checkpoint_best.pt-test.txt \
