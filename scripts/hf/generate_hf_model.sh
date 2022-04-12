641a67ab116be666428508de47350910947f8929
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2022-04-12 12:45:43 +0530

    Harveen | Fix HF script

--- scripts/hf/generate_hf_model.sh
+++ scripts/hf/generate_hf_model.sh
@@ -10,6 +10,8 @@ dict_path=${parentdir}/data/finetuning/dict.ltr.txt
 config_path=${parentdir}/config/hf/hf_config.json
 is_finetuned=1
 
+mkdir $checkpoint_path
+
 if [[ $is_finetuned == 1 ]]; then
 	python ../../utils/hf/convert_to_hf.py -o ${checkpoint_path} -m ${finetuning_path} -d ${dict_path} -c ${config_path}
 else
